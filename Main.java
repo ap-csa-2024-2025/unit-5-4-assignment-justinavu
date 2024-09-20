202c5d3a23988d4f2c486b8c09ae7836342bdb4c
justinavu <justinvu2008@gmail.com>
2024-09-20 10:04:47 -0700

    sync

--- Main.java
+++ Main.java
@@ -2,12 +2,22 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // test your functions here to make sure they work
+    double distance = distance(0,0,5,0);
+    System.out.println("Distance is " + distance);
+    int a = 1;
+    int b = 5;
+    int c = 6;
+    double solution1 = quadPlus(a, b, c);
+    double solution2 = quadMinus(a, b, c);
+    System.out.println(a + ("x^2") + " + " + b + "x + " + c + ", x = " + solution1 + "," + solution2);
   }
-
-  // Implement distance formula method here
-
-  // implement one of your quadratic formula methods here
-
-  // implement the other quadratic formula here
+  public static double distance(double x1, double y1, double x2, double y2) {
+    return Math.sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+  }
+  public static double quadPlus(double a, double b, double c) {
+    return((-b + Math.sqrt((b*b)-(4*a*c)))/2*a);
+  }
+  public static double quadMinus(double a, double b, double c) {
+    return((-b - Math.sqrt(((b*b)-4*a*c)))/2*a);
+}
 }
